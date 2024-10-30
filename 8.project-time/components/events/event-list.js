8d5471bdbdfeef74b68f3752202f0873e69b8418
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2024-10-30 09:48:07 +0700

    add: css module for list and item events

--- 8.project-time/components/events/event-list.js
+++ 8.project-time/components/events/event-list.js
@@ -1,10 +1,11 @@
 import EventItem from './event-item';
+import clasess from './event-list.module.css';
 
 function EventList(props) {
   const { items } = props;
 
   return (
-    <ul>
+    <ul className={clasess.list}>
       {items.map((item) => (
         <EventItem
           key={item.id}
