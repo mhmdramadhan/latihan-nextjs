ce61fc92078b2ec5a4aedb662fddee74c9a8856b
Rama <mr.mhmd.ramadhan@gmail.com>
2024-12-10 15:48:18 +0700

    Add: Konfigurasi getstaticProps

--- 9.page-rendering&data-fetching/pages/index.js
+++ 9.page-rendering&data-fetching/pages/index.js
@@ -13,11 +13,23 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(contex) {
   console.log("regenerating pages...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
