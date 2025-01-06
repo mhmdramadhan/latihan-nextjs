bef88b5547ad672e63d5775416b24bf3c2e6c7cb
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-01-07 05:47:33 +0700

    add: Connect to mongodb

--- 12.project-time-api-routes/pages/api/newsletter.js
+++ 12.project-time-api-routes/pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler (req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email } = req.body;
 
@@ -6,6 +8,16 @@ function handler (req, res) {
       res.status(422).json({ message: 'Invalid email address' });
       return;
     }
+
+    // Connect to MongoDB
+    const client = await MongoClient.connect(
+      'mongodb+srv://ramadhan:MW0SUzn4AuJU1kDb@cluster0.5zt6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    );
+    const db = client.db('newsletter');
+
+    await db.collection('emails').insertOne({ email });
+
+    client.close();
 
     // Store in a database
     res.status(201).json({ message: 'Signed up!' });
