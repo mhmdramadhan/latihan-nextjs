31d2d9eed73a0ee0a65e9ae234bc2393334ffe6f
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-01-14 05:54:07 +0700

    add: context for notification:

---
+++ 12.project-time-api-routes/store/notification-contex.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from 'react';
+
+const NotificationContext = createContext({
+  notification: null,
+  showNotification: (notificationData) => {},
+  hideNotification: () => {},
+});
+
+export function NotificationContextProvider(props) {
+  const showNotificationHandler = (notificationData) => {
+    setNotification(notificationData);
+  };
+
+  const hideNotificationHandler = () => {
+    setNotification(null);
+  };
+
+  const [notification, setNotification] = useState(null);
+
+  const context = {
+    notification: notification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
+
+export default NotificationContext;
