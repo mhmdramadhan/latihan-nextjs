73505f194c9440601e1a333f2cb37ee86152d642
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-02-06 06:27:22 +0700

    ADD : integrate MongoDB for contact form submissions and enhance input validation

--- 13.build-full-blog-app/pages/api/contact.js
+++ 13.build-full-blog-app/pages/api/contact.js
@@ -1,8 +1,20 @@
-function handler (req, res) {
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri =
+  'mongodb+srv://ramadhan:IBxLuKaP6RqsG6I0@cluster0.5zt6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
-    if (!email || !email.includes('@') || !name || name.trim() === '' || !message || message.trim() === '') {
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
       res.status(422).json({ message: 'Invalid input.' });
       return;
     }
@@ -10,12 +22,39 @@ function handler (req, res) {
     const newMessage = {
       email,
       name,
-      message
+      message,
     };
 
-    console.log(newMessage);
+    let client;
+    try {
+      // Connect to the MongoDB cluster
+      client = await MongoClient.connect('mongodb+srv://ramadhan:IBxLuKaP6RqsG6I0@cluster0.5zt6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: 'Could not connect to database!', error: error });
+      return;
+    }
 
-    res.status(201).json({ message: 'Successfully stored message!', message: newMessage });
+    const db = client.db('my-site');
+    const messageCollection = db.collection('messages');
+
+    try {
+      // Insert the new message into the messages collection
+      const result = await messageCollection.insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: 'Storing message failed!', error: error });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: 'Successfully stored message!', message: newMessage });
   }
 }
 
