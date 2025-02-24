119446a77d70a0738a79a2c4e809d6b5b28db7cf
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-02-25 06:03:13 +0700

    UPDATE : refactor syntax highlighting in PostContent component to use PrismLight and register JavaScript and CSS languages

--- 13.build-full-blog-app/components/posts/post-detail/post-content.js
+++ 13.build-full-blog-app/components/posts/post-detail/post-content.js
@@ -1,10 +1,15 @@
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import Image from 'next/image';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
 function PostContent(props) {
   const { post } = props;
@@ -37,7 +42,7 @@ function PostContent(props) {
             />
           </div>
         );
-      }else{
+      } else {
         return <p>{paragraph.children}</p>;
       }
     },
