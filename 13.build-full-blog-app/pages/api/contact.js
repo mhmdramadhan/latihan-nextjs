b9b7c0633d09f3287d967e854616a9f4d370ac5f
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-02-25 05:47:00 +0700

    UPDATE : refactor MongoDB connection in contact API to use environment variables; add next.config.js for environment-specific settings

--- 13.build-full-blog-app/pages/api/contact.js
+++ 13.build-full-blog-app/pages/api/contact.js
@@ -1,7 +1,10 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri =
-  'mongodb+srv://ramadhan:HPuzaJJeMABB15Xf@cluster0.5zt6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// Create a MongoClient with a connection URI that includes the username and password
+const uri = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.5zt6g.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
+// const uri =
+//   'mongodb+srv://ramadhan:HPuzaJJeMABB15Xf@cluster0.5zt6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,7 +33,7 @@ async function handler(req, res) {
 
     try {
       await client.connect();
-      const db = client.db('my-site');
+      const db = client.db();
       const collection = db.collection('messages');
 
       const result = await collection.insertOne({ email, name, message });
