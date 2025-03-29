2f617fa539e0466d0094bab2dc0d25a86129b377
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2025-03-29 20:45:54 +0700

    UPDATE : enhance layout structure; wrap children in Layout component; add metadata for New Meetup page; update Link components to use href

--- 15.final-recap-summary/app/new-meetup/page.js
+++ 15.final-recap-summary/app/new-meetup/page.js
@@ -1,5 +1,11 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 
+export const metadata = {
+    title: "Add New Meetup",
+    description: "Create and add a new meetup to our platform. Share your events with the community!",
+    keywords: ["meetup", "add meetup", "new meetup", "community events"],
+};
+
 function NewMeetupPage() {
     return <NewMeetupForm />
 }
